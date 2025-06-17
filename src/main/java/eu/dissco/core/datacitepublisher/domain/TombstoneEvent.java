457bb3a28a805658c5da8b4e2dd5b354ff574ca4
Soulaine <soulaine.theo@gmail.com>
2025-06-17 13:02:45 +0200

    Qualifiers and version

--- src/main/java/eu/dissco/core/datacitepublisher/domain/TombstoneEvent.java
+++ src/main/java/eu/dissco/core/datacitepublisher/domain/TombstoneEvent.java
@@ -1,9 +1,13 @@
 package eu.dissco.core.datacitepublisher.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.dissco.core.datacitepublisher.domain.datacite.DcRelatedIdentifiers;
 import java.util.List;
 
-public record TombstoneEvent(String handle, List<DcRelatedIdentifiers> dcRelatedIdentifiersTombstone) {
+public record TombstoneEvent(
+    String handle,
+    @JsonProperty("dcRelatedIdentifiers")
+    List<DcRelatedIdentifiers> dcRelatedIdentifiersTombstone) {
 
 
 }
--- src/main/java/eu/dissco/core/datacitepublisher/service/RabbitMqPublisherService.java
+++ src/main/java/eu/dissco/core/datacitepublisher/service/RabbitMqPublisherService.java
@@ -9,6 +9,7 @@ import eu.dissco.core.datacitepublisher.properties.RabbitMqProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class RabbitMqPublisherService {
 
   private final RabbitTemplate rabbitTemplate;
   private final RabbitMqProperties rabbitMqProperties;
+  @Qualifier("objectMapper")
   private final ObjectMapper mapper;
 
   public void deadLetterEventSpecimenDoi(DigitalSpecimenEvent event) {
